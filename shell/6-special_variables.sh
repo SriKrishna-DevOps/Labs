ce478b50f9978f3ff0bc1a1ad47424a0cab1731f
kittu8008 <krishnaveerla95@gmail.com>
2026-02-02 22:49:13 +0530

    your message

--- shell/6-special_variables.sh
+++ shell/6-special_variables.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "script name:$0"
+echo "script name:$0"  
+echo "collects every input argument.:$@"
+ech "no of arguments passed:$#"
+echo "1st argument:$1"
+echo "2t argument:$2"
+
+
+echo "present working directory:$pwd"
+echo "current user:$user"
+echo "home directory of current user:$home"
+
+echo "last command exit status:$?"
+#0 is success
+#non zero is  failure
+
