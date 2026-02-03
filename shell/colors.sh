e02078ef9bb8605084be790ae7746c4ff3dca540
kittu8008 <krishnaveerla95@gmail.com>
2026-02-03 12:11:40 +0530

    “message”

---
+++ shell/colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Regular colors
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+
+# Bright colors
+BRED="\033[91m"
+BGREEN="\033[92m"
+BYELLOW="\033[93m"
+BBLUE="\033[94m"
+BMAGENTA="\033[95m"
+BCYAN="\033[96m"
+BWHITE="\033[97m"
+
+# Styles
+BOLD="\033[1m"
+UNDERLINE="\033[4m"
+
+# Reset
+NC="\033[0m"
+
+
+echo -e "${RED}sri krishna${NC}"
+echo -e "${GREEN}vamsi${NC}"
+echo -e "${YELLOW}subbu${NC}"
+echo -e "${BLUE}koushik${NC}"
+echo -e "${MAGENTA}srinu${NC}"
+echo -e "${CYAN}deepu${NC}"
+ 
